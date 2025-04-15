930245cf486678f6176c0d7e03ec98e9cf55526c
Siyuan Zhang <sizhang@google.com>
2025-04-15 16:23:04 -0700

    convert gpt.py to ray job

---
+++ ray_gpt.py
@@ -0,0 +1,282 @@
+import tempfile
+import os
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+import ray
+import ray.train.torch
+# from ray import train, tune
+# from ray.train import Checkpoint
+# from ray.tune.schedulers import AsyncHyperBandScheduler
+
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
+# hyperparameters
+default_config = {
+"batch_size": 64, # how many independent sequences will we process in parallel?
+"block_size": 256, # what is the maximum context length for predictions?
+"max_iters": 50,
+"eval_interval": 500,
+"learning_rate": 3e-4,
+"eval_iters": 200,
+"n_embd": 128,
+"n_head": 6,
+"n_layer": 6,
+"dropout": 0.2,
+}
+
+# ------------
+
+torch.manual_seed(1337)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split, config):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - config['block_size'], (config['batch_size'],))
+    x = torch.stack([data[i:i+config['block_size']] for i in ix])
+    y = torch.stack([data[i+1:i+config['block_size']+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss(model, config):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(config['eval_iters'])
+        for k in range(config['eval_iters']):
+            X, Y = get_batch(split, config)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size, config):
+        super().__init__()
+        self.key = nn.Linear(config['n_embd'], head_size, bias=False)
+        self.query = nn.Linear(config['n_embd'], head_size, bias=False)
+        self.value = nn.Linear(config['n_embd'], head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(config['block_size'], config['block_size'])))
+
+        self.dropout = nn.Dropout(config['dropout'])
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size, config):
+        super().__init__()
+
+        self.heads = nn.ModuleList([Head(head_size, config) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, config['n_embd'])
+        self.dropout = nn.Dropout(config['dropout'])
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, config):
+        super().__init__()
+        n_embd = config['n_embd']
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(config['dropout']),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, config):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        n_embd = config['n_embd']
+        n_head = config['n_head']
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size, config)
+        self.ffwd = FeedFoward(config)
+        self.ln1 = nn.LayerNorm(config['n_embd'])
+        self.ln2 = nn.LayerNorm(config['n_embd'])
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+class GPTLanguageModel(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, config['n_embd'])
+        self.position_embedding_table = nn.Embedding(config['block_size'], config['n_embd'])
+        self.blocks = nn.Sequential(*[Block(config) for _ in range(config['n_layer'])])
+        self.ln_f = nn.LayerNorm(config['n_embd']) # final layer norm
+        self.lm_head = nn.Linear(config['n_embd'], vocab_size)
+        self.block_size = config['block_size']
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -self.block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+    
+
+def train_gpt(config=default_config):
+    model = GPTLanguageModel(config)
+    model = ray.train.torch.prepare_model(model)
+    m = model.to(device)
+    # print the number of parameters in the model
+    print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config["learning_rate"])
+
+    for iter in range(config['max_iters']):
+
+        # every once in a while evaluate the loss on train and val sets
+        if iter % config['eval_interval'] == 0 or iter == config['max_iters'] - 1:
+            losses = estimate_loss(model, config)
+            train_loss = f"{losses['train']:.4f}"
+            val_loss = f"{losses['val']:.4f}"
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+             # [3] Report metrics and checkpoint.
+            metrics = {"train_loss": float(train_loss), "val_loss": float(val_loss), "iter": iter}
+            # with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
+            #     torch.save(
+            #         model.module.state_dict(),
+            #         os.path.join(temp_checkpoint_dir, "model.pt")
+            #     )
+            ray.train.report(
+                metrics,
+                # checkpoint=ray.train.Checkpoint.from_directory(temp_checkpoint_dir),
+            )
+            if ray.train.get_context().get_world_rank() == 0:
+                print(metrics)
+
+        # sample a batch of data
+        xb, yb = get_batch('train', config)
+
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+    # return m
+
+
+# [4] Configure scaling and resource requirements.
+resources_per_worker = {"CPU": 4, "GPU": 0}
+scaling_config = ray.train.ScalingConfig(num_workers=3, resources_per_worker=resources_per_worker)
+
+# [5] Launch distributed training job.
+trainer = ray.train.torch.TorchTrainer(
+    train_gpt,
+    # run_config=ray.train.RunConfig(
+    #     storage_path="gs://sizhang-ray-experimental/ray-experimental/",
+    #     name="ray_gpt",
+    # ),
+    scaling_config=scaling_config,
+    # [5a] If running in a multi-node cluster, this is where you
+    # should configure the run's persistent storage that is accessible
+    # across all worker nodes.
+    # run_config=ray.train.RunConfig(storage_path="s3://..."),
+)
+result = trainer.fit()
+
+# # # generate from the model
+# m = train_gpt(default_config)
+# context = torch.zeros((1, 1), dtype=torch.long, device=device)
+# print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
